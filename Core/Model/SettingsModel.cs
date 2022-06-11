28c318d24e50533c89fb9459cdac383208adb4e9
3alTemp <trialtemp.mailer@gmail.com>
2022-06-11 12:02:00 -0500

    Compressed

--- Core/Model/SettingsModel.cs
+++ Core/Model/SettingsModel.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Twiforked.Core.Model
+﻿namespace Twiforked.Core.Model
 {
-	class SettingsModel
-	{
-	}
+	class SettingsModel { }
 }
