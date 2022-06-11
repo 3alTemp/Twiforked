e855ba1140898947afab322d8f211e0a15e7da68
3alTemp <trialtemp.mailer@gmail.com>
2022-06-11 04:27:42 -0500

    Organized + fixed Model/View

--- Core/Model/SettingsModel.cs
+++ Core/Model/SettingsModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Twiforked.Core.ViewModel
+namespace Twiforked.Core.Model
 {
-	class SettingsViewModel
+	class SettingsModel
 	{
 	}
 }
--- Core/View/SettingsView.xaml.cs
+++ Core/View/SettingsView.xaml.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Twiforked.Tabs
+namespace Twiforked.Core.View
 {
 	/// <summary>
 	/// Interaction logic for SettingsView.xaml
 	/// </summary>
 	public partial class SettingsView : UserControl
 	{
-		public SettingsView()
-		{
-			InitializeComponent();
-		}
 	}
 }
