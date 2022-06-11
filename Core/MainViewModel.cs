211c43a1f4099f4e55824648e9678b6db5cbe107
3alTemp <trialtemp.mailer@gmail.com>
2022-06-11 12:03:19 -0500

    Finished writing ViewModel code

--- Core/MainViewModel.cs
+++ Core/MainViewModel.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Twiforked.Core.Model;
 
 namespace Twiforked.Core
 {
 	class MainViewModel : ObservableObject
 	{
+		public SettingsModel _settingsModel { get; set; }
+
+		
+		private object _currentView;
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set
+			{
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
 		public MainViewModel()
 		{
-
+			_settingsModel = new SettingsModel();
+			CurrentView = _settingsModel;
 		}
 	}
 }
